2409ebc49a6d693b8e4770b39bbead632d75c787
Yash <yashpandey1106@gmail.com>
2023-03-08 17:05:26 +0530

    implemented user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  root 'home#index'
   devise_for :users
   resources :users
-  get 'registrations/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :registrations
+  post '/signup', to: 'registrations#signup', as: :signup
+  get '/signup', to: 'registrations#new', as: :new_signup
+
+  resources :sessions
+  post '/login', to: 'sessions#login', as: :login
+  get '/login', to: 'sessions#new', as: :new_login
+  delete '/logout', to: 'sessions#logout', as: :logout
 end
