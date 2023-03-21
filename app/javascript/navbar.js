5151a2ec5962c117e6f064093d8acd294febc189
Yash <yashpandey1106@gmail.com>
2023-03-21 23:14:22 +0530

    responsive navbar

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,5 +1,12 @@
 const profileImage = document.querySelector('.profile-image');
 const dropdownMenu = document.querySelector('.dropdown-menu');
+const hamBurger = document.querySelector('.ham-burger');
+const dropdown = document.querySelector('.dropdown');
 profileImage.addEventListener('click', function() {
   dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
 });
+
+
+hamBurger.addEventListener('click', function() {
+  dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+})
